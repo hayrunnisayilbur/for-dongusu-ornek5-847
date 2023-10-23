29e23cbbd16ee02cfb1823a109e7434791b7b19c
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-10-23 14:56:00 +0300

    Add files via upload

---
+++ for dongusu ornek5- 847/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace for_dongusu_ornek5__847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.lbSayilar = new System.Windows.Forms.ListBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtDegisim = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(131, 77);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(136, 20);
+            this.txtSayi1.TabIndex = 0;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(131, 112);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(136, 20);
+            this.txtSayi2.TabIndex = 1;
+            // 
+            // lbSayilar
+            // 
+            this.lbSayilar.FormattingEnabled = true;
+            this.lbSayilar.Location = new System.Drawing.Point(285, 77);
+            this.lbSayilar.Name = "lbSayilar";
+            this.lbSayilar.Size = new System.Drawing.Size(146, 238);
+            this.lbSayilar.TabIndex = 2;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(131, 193);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(136, 45);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.Text = "ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txtDegisim
+            // 
+            this.txtDegisim.Location = new System.Drawing.Point(131, 153);
+            this.txtDegisim.Name = "txtDegisim";
+            this.txtDegisim.Size = new System.Drawing.Size(136, 20);
+            this.txtDegisim.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDegisim);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.lbSayilar);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.ListBox lbSayilar;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.TextBox txtDegisim;
+    }
+}
+
---
+++ for dongusu ornek5- 847/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_dongusu_ornek5__847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            lbSayilar.Items.Clear();
+            int sayi1 = Convert.ToInt32(txtSayi1.Text);
+            int sayi2 = Convert.ToInt32(txtSayi2.Text);
+            int sayi3 = Convert.ToInt32(txtDegisim.Text);
+            for(int i = sayi1; i<= sayi2; i = i+ sayi3)
+            {
+                lbSayilar.Items.Add(i);
+            }
+
+            
+            }
+        }
+    }
+
